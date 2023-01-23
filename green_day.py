4d509cf54cb90997883035b3bfd98504fb2b6608
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2023-01-22 22:01:26 -0600

    feat(green_day.py): gathers data from multiple stocks

--- green_day.py
+++ green_day.py
@@ -11,36 +11,69 @@ plt.rcParams["text.color"] = "#EBDBB2"
 plt.rcParams["xtick.color"] = "#EBDBB2"
 plt.rcParams["ytick.color"] = "#EBDBB2"
 
-aapl = yf.Ticker("AAPL")
-
-df = aapl.history("max")
-
-df["Green"] = df["Close"].diff() > 0
-
 
 def consecutive(window, total, runs):
+    """
+
+    Args:
+        window: window of elements to look over
+        total: current total number of samples viewed
+        runs: current number of runs found
+
+    Returns:
+        total, runs
+
+    """
     if all(day for day in window[0:-1]):
         if window[-1]:
             return total + 1, runs + 1
-        else:
-            return total + 1, runs
-    else:
-        return total, runs
+        return total + 1, runs
+    return total, runs
 
 
-def sliding_window(elements, window_size):
+def sliding_window(elements, window_size, run_function):
+    """
+
+    Args:
+        elements: elements to apply sliding window to
+        window_size: sliding window size
+        run_function: function that calculates number of runs
+
+    Returns:
+        number of runs and total number of samples taken
+
+    """
     total = 0
     runs = 0
     length = len(elements)
     if length <= window_size:
         return elements
-    for idx, row in enumerate(elements):
+    for idx, _ in enumerate(elements):
         if idx < length - window_size:
-            total, runs = consecutive(elements[idx : idx + window_size], total, runs)
+            total, runs = run_function(elements[idx : idx + window_size], total, runs)
     return runs, total
 
 
-def show_results(num_consecutive_days):
-    runs, total = sliding_window(df["Green"], num_consecutive_days)
-    percentage = runs / total
-    return percentage, runs
+def get_results(num_consecutive_days, green_df):
+    """
+
+    Args:
+        num_consecutive_days: number of days used to calculate a run
+        green_df: dataframe marking green days
+
+    Returns:
+        chance: chance of a green day after num_consecutive_days of a run
+
+    """
+    green_df.dropna(inplace=True)
+    runs, total = sliding_window(green_df, num_consecutive_days, consecutive)
+    chance = runs / total
+    return chance, runs
+
+
+multi = yf.Tickers("AAPL TSLA")
+df = multi.history("max")
+df = df["Close"]
+for col_num, item in enumerate(df.items()):
+    df[f"Green {item[0]}"] = df[item[0]].diff() > 0
+    print(get_results(4, df[f"Green {item[0]}"]))
